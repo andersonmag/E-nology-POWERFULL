71236180334bf4d866c63fe48de98e5d6e35b8fd
Anderson <adnrepper@gmail.com>
2019-10-05 00:22:43 -0300

    Configuração h2

--- src/main/java/br/edu/ifal/enology/DemoApplication.java
+++ src/main/java/br/edu/ifal/enology/DemoApplication.java
@@ -2,8 +2,10 @@ package br.edu.ifal.enology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
