08dda5909787015f558ef4ab6599704317a63abc
Anderson <adnrepper@gmail.com>
2019-10-08 19:41:13 -0300

    Modificações nas páginas para cadastrar e editar perfil

--- src/main/java/br/edu/ifal/enology/DemoApplication.java
+++ src/main/java/br/edu/ifal/enology/DemoApplication.java
@@ -2,10 +2,8 @@ package br.edu.ifal.enology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
