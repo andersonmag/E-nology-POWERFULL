08824dd2a5737ff372846fd7f792a98d6d471332
Anderson <adnrepper@gmail.com>
2019-10-08 19:39:35 -0300

    Mapeamento das páginas e funções de salvar e atualizar

--- src/main/java/br/edu/ifal/enology/controller/Controller.java
+++ src/main/java/br/edu/ifal/enology/controller/Controller.java
@@ -1,18 +1,81 @@
 package br.edu.ifal.enology.controller;
 
+import java.util.Optional;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import br.edu.ifal.enology.model.Usuario;
+import br.edu.ifal.enology.repository.RepositoryUser;
 
 @RestController
 public class Controller{
 
-    @RequestMapping("/")
+    @Autowired
+    RepositoryUser rep;
+
+	@RequestMapping("/")
     public ModelAndView index(){
 
-        ModelAndView model = new ModelAndView("index");
+        return new ModelAndView("index");        
+    }
+
+    @RequestMapping("/cadastro")
+    public ModelAndView cadastro(){
+
+        return new ModelAndView("user/cadastro");
+    }
+
+    @RequestMapping("/mapa")
+    public ModelAndView mostrarMapa(){
+
+        return new ModelAndView("map/mapa.html");
+    }
+
+    @RequestMapping("/perfil")
+    public ModelAndView mostrarPerfil(@CookieValue(name = "id", defaultValue = "") Long IdCookie){
+        ModelAndView model = new ModelAndView("user/perfil");
+        Optional<Usuario> opcao = rep.findById(IdCookie);
+
+        if(opcao.isPresent()){
+
+            Usuario usuario = opcao.get();
+            model.addObject("usuario", usuario);
+
+            return model;
+        }
+
         return model;
-        
+    }
+
+    @RequestMapping("/salvar")
+    public ModelAndView salvar(@Valid Usuario usuario, HttpServletResponse response){
+
+        rep.save(usuario);
+        Cookie cookie = new Cookie("id", usuario.getId().toString());
+        response.addCookie(cookie);
+
+        return new ModelAndView("redirect:/perfil");
+    }
+
+    @RequestMapping("/editarPerfil")
+    public ModelAndView editarPerfil(@CookieValue(name = "id", defaultValue = "") Long IdCookie){
+        ModelAndView model = new ModelAndView("user/editarDadosPerfil");
+        Optional<Usuario> opcao = rep.findById(IdCookie);
+
+        if(opcao.isPresent()){
+
+            Usuario usuario = opcao.get();
+            model.addObject("usuario", usuario);
+
+            return model;
+        }
+
+        return model;
     }
 
 }
