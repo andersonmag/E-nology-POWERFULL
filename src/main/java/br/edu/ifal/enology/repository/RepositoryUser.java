ec6cf4e615b34c38b31262d98505e87f32d5f0be
Anderson <adnrepper@gmail.com>
2019-10-08 19:38:03 -0300

    Repositorio criado

---
+++ src/main/java/br/edu/ifal/enology/repository/RepositoryUser.java
@@ -0,0 +1,8 @@
+package br.edu.ifal.enology.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import br.edu.ifal.enology.model.Usuario;
+
+public interface RepositoryUser extends CrudRepository<Usuario, Long>{
+
+}
