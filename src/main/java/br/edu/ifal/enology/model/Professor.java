17ec84cd9862b84a4d7cecff8b609e32b7d8a8ad
Anderson <adnrepper@gmail.com>
2019-11-22 21:34:15 -0300

    Botão de logout no mapa/Interface UserDetails:Usuario

--- src/main/java/br/edu/ifal/enology/model/Professor.java
+++ src/main/java/br/edu/ifal/enology/model/Professor.java
@@ -4,5 +4,7 @@ import javax.persistence.Entity;
 
 @Entity
 public class Professor extends Usuario{
+
+    private static final long serialVersionUID = 1L;
     
 }
--- src/main/java/br/edu/ifal/enology/model/Usuario.java
+++ src/main/java/br/edu/ifal/enology/model/Usuario.java
@@ -1,12 +1,17 @@
 package br.edu.ifal.enology.model;
 
+import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario{
+public class Usuario implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -54,5 +59,39 @@ public class Usuario{
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
-    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
