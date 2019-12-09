df39585ab735e8505d1f1f4d2e2118d23dadd42a
Jackson Silva <jackson.silva.who@gmail.com>
2019-12-09 16:19:02 -0300

    Adicionada classe modelo Licao para testes + mapeamentos no TaskController

--- src/main/java/br/edu/ifal/enology/controller/TaskController.java
+++ src/main/java/br/edu/ifal/enology/controller/TaskController.java
@@ -1,25 +1,144 @@
 package br.edu.ifal.enology.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import br.edu.ifal.enology.model.Licao;
 
 @RestController
-public class TaskController{
+public class TaskController {
+    Licao licao = new Licao();
+    boolean terminouPergunta1 = false;
+    boolean terminouPergunta2 = false;
+    boolean terminouPergunta3 = false;
+    boolean terminouPergunta4 = false;
+    boolean terminouPergunta5 = false;
+    int i = 0;
 
     @RequestMapping("/licao")
     public ModelAndView licao() {
-        return new ModelAndView("task/licao1");
+        ModelAndView pagina = new ModelAndView("task/licao1");
+
+        switch (i) {
+        case 0:
+            pagina.addObject("enunciado", licao.getEnunciados().get(i));
+            pagina.addObject("palavras", licao.getPalavrasPergunta1());
+            terminouPergunta1 = true;
+            break;
+        case 1:
+            pagina.addObject("enunciado", licao.getEnunciados().get(i));
+            pagina.addObject("palavras", licao.getPalavrasPergunta2());
+            terminouPergunta2 = true;
+            break;
+        case 2:
+            pagina.addObject("enunciado", licao.getEnunciados().get(i));
+            pagina.addObject("palavras", licao.getPalavrasPergunta3());
+            terminouPergunta3 = true;
+            break;
+        case 3:
+            pagina.addObject("enunciado", licao.getEnunciados().get(i));
+            pagina.addObject("palavras", licao.getPalavrasPergunta4());
+            terminouPergunta4 = true;
+            break;
+        case 4:
+            pagina.addObject("enunciado", licao.getEnunciados().get(i));
+            pagina.addObject("palavras", licao.getPalavrasPergunta5());
+            terminouPergunta5 = true;
+            break;
+        }
+
+        if (i > 4) {
+            pagina = new ModelAndView("task/final");
+            terminouPergunta1 = false;
+            terminouPergunta2 = false;
+            terminouPergunta3 = false;
+            terminouPergunta4 = false;
+            i = 0;
+        }
+        i++;
+        return pagina;
     }
 
-    @RequestMapping("/licao2")
-    public ModelAndView licao2() {
-        return new ModelAndView("task/licao2.html");
+    @RequestMapping("/corrigir/{palavra}")
+    public ModelAndView verificar(@PathVariable("palavra") String palavra) {
+        ModelAndView pagina;
+        if (terminouPergunta1) {
+            if (licao.conferirResposta1(palavra)) {
+                pagina = new ModelAndView("task/correto");
+                terminouPergunta1 = false;
+            } else {
+                pagina = new ModelAndView("task/errado");
+                terminouPergunta1 = false;
+            }
+
+        } else if (terminouPergunta2) {
+            if (licao.conferirResposta2(palavra)) {
+                pagina = new ModelAndView("task/correto");
+                terminouPergunta2 = false;
+            } else {
+                pagina = new ModelAndView("task/errado");
+                terminouPergunta2 = false;
+            }
+        } else if (terminouPergunta3) {
+            if (licao.conferirResposta3(palavra)) {
+                pagina = new ModelAndView("task/correto");
+                terminouPergunta3 = false;
+            } else {
+                pagina = new ModelAndView("task/errado");
+                terminouPergunta3 = false;
+            }
+        } else if (terminouPergunta4) {
+            if (licao.conferirResposta4(palavra)) {
+                pagina = new ModelAndView("task/correto");
+                terminouPergunta4 = false;
+            } else {
+                pagina = new ModelAndView("task/errado");
+                terminouPergunta4 = false;
+            }
+        } else if (terminouPergunta5) {
+            if (licao.conferirResposta5()) {
+                pagina = new ModelAndView("task/correto");
+                terminouPergunta4 = false;
+            } else {
+                pagina = new ModelAndView("task/errado");
+                terminouPergunta4 = false;
+            }
+        } else {
+            pagina = new ModelAndView("redirect:/mapa");
+        }
+
+        return pagina;
     }
 
-    @RequestMapping("/licao3")
-    public ModelAndView licao3() {
-        return new ModelAndView("task/licao3.html");
+    @RequestMapping("/cancelar")
+    public ModelAndView cancelar() {
+        i = 0;
+        terminouPergunta1 = false;
+        terminouPergunta2 = false;
+        terminouPergunta3 = false;
+        terminouPergunta4 = false;
+        return new ModelAndView("redirect:/mapa");
     }
+
+    @RequestMapping("/finalizar")
+    public ModelAndView finalizar() {
+        i = 0;
+        terminouPergunta1 = false;
+        terminouPergunta2 = false;
+        terminouPergunta3 = false;
+        terminouPergunta4 = false;
+        return new ModelAndView("redirect:/mapa");
+    }
+
+    // @RequestMapping("/licao2")
+    // public ModelAndView licao2() {
+    //     return new ModelAndView("task/licao2.html");
+    // }
+
+    // @RequestMapping("/licao3")
+    // public ModelAndView licao3() {
+    //     return new ModelAndView("task/licao3.html");
+    // }
 
 }
---
+++ src/main/java/br/edu/ifal/enology/model/Licao.java
@@ -0,0 +1,101 @@
+package br.edu.ifal.enology.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Licao {
+    private List<String> palavras;
+    private List<String> enunciados;
+    private List<String> respostas;
+    String[] opcoes;
+
+    public Licao() {
+        inicializarListaPalavras();
+        inicializarListaEnunciados();
+        inicializarListaRespostas();
+        opcoes = new String[4];
+    }
+
+    private void inicializarListaPalavras() {
+        palavras = Arrays.asList("while", "if", "for", "else", "switch-case", "else", "break", "continue", "void",
+                "int", "double", "char", "cobol", "assembly", "prolog", "html", "me", "myself", "I", "idk");
+    }
+
+    private void inicializarListaEnunciados() {
+        enunciados = Arrays.asList("Which of these is a conditional structure?",
+                "Which of these is used to stop a repeat loop?",
+                "Using your knowledge, which of these is used for floating numbers?",
+                "Which of these is not a programming language?", "which of these is the smartest student?");
+    }
+
+    private void inicializarListaRespostas() {
+        respostas = Arrays.asList("if", "break", "double", "html");
+    }
+
+    public List<String> getEnunciados() {
+        return enunciados;
+    }
+
+    public String[] getPalavrasPergunta1() {
+        for (int i = 0; i < opcoes.length; i++) {
+            opcoes[i] = palavras.get(i);
+        }
+        return opcoes;
+    }
+
+    public String[] getPalavrasPergunta2() {
+        int j = 4;
+        for (int i = 0; i < opcoes.length; i++) {
+            opcoes[i] = palavras.get(j);
+            j++;
+        }
+        return opcoes;
+    }
+
+    public String[] getPalavrasPergunta3() {
+        int j = 8;
+        for (int i = 0; i < opcoes.length; i++) {
+            opcoes[i] = palavras.get(j);
+            j++;
+        }
+        return opcoes;
+    }
+
+    public String[] getPalavrasPergunta4() {
+        int j = 12;
+        for (int i = 0; i < opcoes.length; i++) {
+            opcoes[i] = palavras.get(j);
+            j++;
+        }
+        return opcoes;
+    }
+
+    public String[] getPalavrasPergunta5() {
+        int j = 16;
+        for (int i = 0; i < opcoes.length; i++) {
+            opcoes[i] = palavras.get(j);
+            j++;
+        }
+        return opcoes;
+    }
+
+    public boolean conferirResposta1(String palavra) {
+        return palavra.equals(respostas.get(0));
+    }
+
+    public boolean conferirResposta2(String palavra) {
+        return palavra.equals(respostas.get(1));
+    }
+
+    public boolean conferirResposta3(String palavra) {
+        return palavra.equals(respostas.get(2));
+    }
+
+    public boolean conferirResposta4(String palavra) {
+        return palavra.equals(respostas.get(3));
+    }
+
+    public boolean conferirResposta5() {
+        return true;
+    }
+}
