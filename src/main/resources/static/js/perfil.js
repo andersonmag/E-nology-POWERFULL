d07c3748ed5844a1883cd9fb3680540b0a1a4195
Anderson <adnrepper@gmail.com>
2020-01-11 18:25:16 -0300

    Página de editarPerfil excluida, edição e vizualização de perfil na página de Perfil

---
+++ src/main/resources/static/js/perfil.js
@@ -0,0 +1,24 @@
+
+location.href = "#veim";
+
+if (document.getElementById("erroNovaSenha").textContent != "" ||
+    document.getElementById("erroSenhaAtual").textContent != "") {
+
+    document.getElementById("nav1").style.display = "none";
+    document.getElementById("nav2").style.display = "block";
+}
+
+function mudarNav() {
+
+    if (document.getElementById("nav1").style.display == "block") {
+
+        document.getElementById("nav1").style.display = "none";
+        document.getElementById("nav2").style.display = "block";
+    }
+
+    else {
+
+        document.getElementById("nav2").style.display = "none";
+        document.getElementById("nav1").style.display = "block";
+    }
+}
