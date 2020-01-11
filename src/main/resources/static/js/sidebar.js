747e576109ec0816caed7d3751ab4d1ffa69f56c
Anderson <adnrepper@gmail.com>
2020-01-11 18:42:51 -0300

    Arquivo .js do sidebar das páginas

---
+++ src/main/resources/static/js/sidebar.js
@@ -0,0 +1,12 @@
+
+function mostrarSidebar() {
+
+    if (document.getElementById("mySidenav").style.width == "250px") {
+
+        document.getElementById("mySidenav").style.width = "0";
+    }
+    else {
+
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+}
