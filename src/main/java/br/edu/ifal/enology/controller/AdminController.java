3937b20b75de9c3c59337d891e5282367be36c09
Anderson <adnrepper@gmail.com>
2020-01-17 20:32:33 -0300

    mapeamento da página de criação de tarefas agora no controlador Admin

--- src/main/java/br/edu/ifal/enology/controller/AdminController.java
+++ src/main/java/br/edu/ifal/enology/controller/AdminController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import br.edu.ifal.enology.model.Conteudo;
 import br.edu.ifal.enology.model.Palavra;
-import br.edu.ifal.enology.model.Tarefa;
 import br.edu.ifal.enology.model.Usuario;
 import br.edu.ifal.enology.repository.ConteudoRepository;
 import br.edu.ifal.enology.repository.PalavraRepository;
-import br.edu.ifal.enology.repository.TarefaRepository;
 
 @RestController
 public class AdminController {
@@ -19,29 +17,17 @@ public class AdminController {
     @Autowired
     PalavraRepository palavraRepository;
     @Autowired
-    TarefaRepository tarefaRepository;
-    @Autowired
     ConteudoRepository conteudoRepository;
+    Usuario usuarioLogado;
 
-    @RequestMapping("/cadastro_tarefa")
+    @RequestMapping("/tarefa")
     public ModelAndView cadastro_tarefa(Palavra palavra, Authentication authentication) {
-        Usuario usuario = (Usuario) authentication.getPrincipal();
+        usuarioLogado = (Usuario) authentication.getPrincipal();
         ModelAndView model = new ModelAndView("user/tarefas");
         Iterable<Palavra> palavras = palavraRepository.findAll();
         Iterable<Conteudo> conteudos = conteudoRepository.findAll();
-        model.addObject("palavras", palavras).addObject("conteudos", conteudos).addObject("usuario", usuario);
+
+        model.addObject("palavras", palavras).addObject("conteudos", conteudos).addObject("usuario", usuarioLogado);
         return model;
     }
-
-    @RequestMapping("/cadastrar")
-    public ModelAndView cadastrar(Palavra palavra, Tarefa tarefa) {
-        if (palavra.getIngles() != null) {
-            palavraRepository.save(palavra);
-        } else {
-            tarefaRepository.save(tarefa);
-        }
-
-        return new ModelAndView("redirect:/cadastro_tarefa");
-    }
-
 }
