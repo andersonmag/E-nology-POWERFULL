d9ebfef56792a9abb5d71c9a7d2fb8a6b4f727b2
Anderson <adnrepper@gmail.com>
2020-01-26 19:56:57 -0300

    Modificados arquivos .css, .js e .html da página de lição. Ampulheta, modal's para retorno de acerto, erro e timeout + verificação js da resposta do usuario

--- src/main/resources/static/js/licao.js
+++ src/main/resources/static/js/licao.js
@@ -1,23 +1,43 @@
 
-var tempo = new Number(60);
-
-function iniciarCronometro() {
-
-    if ((tempo - 1) >= 0) {
-        tempo = tempo - 1;
-
-        if (tempo < 10) {
-            relogioDoFaustao.innerText = '00:0' + tempo;
-            setTimeout('iniciarCronometro()', 1000);
-
-        } else {
-            relogioDoFaustao.innerText = '00:' + tempo;
-            setTimeout('iniciarCronometro()', 1000);
-
-        }
-    }
-}
+var tempo = new Number(5);
 
 function mudarButao() {
     butao.disabled = false;
 }
+
+function timeout() {
+    $('#exampleModalCenter').modal('show');
+    document.getElementById("music").play();
+}
+
+function iniciarCronometro() {
+    var ampulheta = document.getElementById("ampulheta");
+
+    if ((tempo - 1) >= 0) {
+        tempo = tempo - 1;
+
+        switch (tempo) {
+            case 25:
+                ampulheta.classList.remove('fa-hourglass-1');
+                ampulheta.classList.add('fa-hourglass-2');
+                break;
+            case 1:
+                ampulheta.classList.remove('fa-hourglass-2');
+                ampulheta.classList.add('fa-hourglass-3');
+                break;
+        }
+
+        if (tempo < 10) {
+            relogioDoFaustao.innerText = '00:0' + tempo;
+            setTimeout('iniciarCronometro()', 1000);
+        }
+        else {
+            relogioDoFaustao.innerText = '00:' + tempo;
+            setTimeout('iniciarCronometro()', 1000);
+        }
+    }
+    else {
+        timeout();
+    }
+}
+
