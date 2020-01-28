1c84f1445b4a9599cc5220e178a109a5527b714c
Jackson Silva <jackson.silva.who@gmail.com>
2020-01-27 22:07:08 -0300

    Alterações nos métodos licao e corrigirResposta

--- src/main/java/br/edu/ifal/enology/controller/TaskController.java
+++ src/main/java/br/edu/ifal/enology/controller/TaskController.java
@@ -1,7 +1,7 @@
 package br.edu.ifal.enology.controller;
 
 import java.util.List;
-import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +31,7 @@ public class TaskController {
     SequenciadorService sequenciadorService;
     Tarefa tarefa;
     Usuario usuarioLogado;
+    Long resposta;
 
     @RequestMapping("/cadastrar")
     public ModelAndView cadastrar(Palavra palavra, Tarefa tarefa) {
@@ -46,10 +47,13 @@ public class TaskController {
     @RequestMapping("/corrigir")
     public ModelAndView corrigirResposta(Long palavra, RedirectAttributes redirect) {
         ModelAndView model = new ModelAndView("redirect:/licao/condicionais");
-        System.out.println(palavra);
+        resposta = palavra;
         if (tarefa.getResposta().getId().equals(palavra)) {
             usuarioLogado.setPontuacaoDoAluno(usuarioLogado.getPontuacaoDoAluno() + tarefa.getPontuacao());
-        }
+            sequenciadorService.adicionarTarefaJaRespondida(tarefa);
+            System.out.println(tarefa.getEnunciado());
+        }        
+        
         redirect.addFlashAttribute("resposta", palavra);
         return model;
     }
@@ -59,14 +63,23 @@ public class TaskController {
         ModelAndView model = new ModelAndView("task/licao1");
         usuarioLogado = (Usuario) authentication.getPrincipal();
         // Random random = new Random();
-        List<Tarefa> tarefas = tarefaRepository.findAll();
+        // List<Tarefa> tarefas = tarefaRepository.findAll();
         // int indexSorteio = random.nextInt(tarefas.size());
-        tarefa = tarefas.get(1);
-        List<Palavra> palavrasEncontradas = sequenciadorService.buscarPalavrasPorConteudo("condicionais",
-                tarefa.getResposta().getIngles());
-
-        model.addObject("tarefa", tarefa).addObject("palavras", palavrasEncontradas).addObject("usuario",
-                usuarioLogado);
+        try {      
+          //System.out.println(resposta);
+            if(resposta == null){
+                tarefa = sequenciadorService.buscarTarefa();
+            }
+            
+            resposta = null;
+            List<Palavra> palavrasEncontradas = sequenciadorService.buscarPalavrasPorConteudo(
+                                                "condicionais", tarefa.getResposta().getIngles());
+            model.addObject("tarefa", tarefa)
+                 .addObject("palavras", palavrasEncontradas)
+                 .addObject("usuario", usuarioLogado);
+        } catch (NullPointerException e) {
+            model = new ModelAndView("redirect:/mapa");
+        }
         return model;
     }
 }
