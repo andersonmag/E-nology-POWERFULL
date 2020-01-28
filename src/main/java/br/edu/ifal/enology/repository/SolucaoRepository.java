30d81a458cdac32ff3ce6906093bc0584356c85e
Priscylla Silva <39808602+priscyllaifal@users.noreply.github.com>
2020-01-28 16:06:52 -0300

    Create SolucaoRepository.java

---
+++ src/main/java/br/edu/ifal/enology/repository/SolucaoRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ifal.enology.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import br.edu.ifal.enology.model.Solucao;
+
+public interface SolucaoRepository extends CrudRepository<Solucao, Long>{
+
+    
+}
