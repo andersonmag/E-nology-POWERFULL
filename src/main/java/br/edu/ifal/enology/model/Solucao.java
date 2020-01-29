d0c05ea7c4bc3d3bfb01a1b849d7b131abb8228e
Anderson <adnrepper@gmail.com>
2020-01-28 22:59:46 -0300

    Resolução de bug na classe de Solucao

--- src/main/java/br/edu/ifal/enology/model/Solucao.java
+++ src/main/java/br/edu/ifal/enology/model/Solucao.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 
 @Entity
 public class Solucao{
@@ -16,7 +14,6 @@ public class Solucao{
     private Long id;
 
     @OneToOne
-    @Cascade(CascadeType.ALL)
     private Usuario aluno;
     @OneToOne
     private Tarefa tarefa;
