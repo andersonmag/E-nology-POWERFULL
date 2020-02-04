f6fe92892cc75824df727a8d5704bcf26b3fcd92
Anderson <adnrepper@gmail.com>
2020-02-04 01:18:58 -0300

    'Criptografia' em informações de verificação da lição

--- src/main/resources/static/js/licao.js
+++ src/main/resources/static/js/licao.js
@@ -1,13 +1,32 @@
 
 var tempo = new Number(50);
 
+function init(rt, r) {
+    var opcoes = document.getElementsByName("palavra");
+
+    for (var i = 0; i <= opcoes.length; i++) {
+        if (opcoes[i].value == r) {
+            if (rt == r) {
+                opcoes[i].checked = true;
+                $('#modalAc').modal('show');
+                document.getElementById("ac").play();
+            }
+            else {
+                opcoes[i].checked = true;
+                $('#modalEr').modal('show');
+                document.getElementById("er").play();
+            }
+        }
+    }
+}
+
 function mudarButao() {
     butao.disabled = false;
 }
 
 function timeout() {
     $('#exampleModalCenter').modal('show');
-    document.getElementById("somTimeout").play();
+    document.getElementById("timeout").play();
 }
 
 function iniciarCronometro() {
