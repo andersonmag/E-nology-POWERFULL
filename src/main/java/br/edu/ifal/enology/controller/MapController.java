5fbd5d84a040965450a8d35806d7049fe17685a8
Anderson <adnrepper@gmail.com>
2020-02-09 16:13:32 -0300

    Recebendo Usuario e Tarefa em cada metodo, resolvendo bug de variaveis globais.

--- src/main/java/br/edu/ifal/enology/controller/MapController.java
+++ src/main/java/br/edu/ifal/enology/controller/MapController.java
@@ -1,7 +1,5 @@
 package br.edu.ifal.enology.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +10,8 @@ import br.edu.ifal.enology.model.Usuario;
 public class MapController {
 
     @RequestMapping("/mapa")
-    public ModelAndView mostrarMapa(@AuthenticationPrincipal Usuario usuarioLogado, HttpServletRequest request) {
+    public ModelAndView mostrarMapa(@AuthenticationPrincipal Usuario usuarioLogado) {
         ModelAndView model = new ModelAndView("map/mapa");
-        request.getSession().setAttribute("usuarioLogado", usuarioLogado);
 
         model.addObject("usuario", usuarioLogado);
         return model;
