853756a0efab2806d8d16decb8acb9303b70e03c
Anderson <adnrepper@gmail.com>
2020-02-10 23:38:22 -0300

    Admin sendo redirecionado para /tarefa ao logar

--- src/main/java/br/edu/ifal/enology/controller/MapController.java
+++ src/main/java/br/edu/ifal/enology/controller/MapController.java
@@ -1,5 +1,6 @@
 package br.edu.ifal.enology.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,13 @@ import br.edu.ifal.enology.model.Usuario;
 public class MapController {
 
     @RequestMapping("/mapa")
-    public ModelAndView mostrarMapa(@AuthenticationPrincipal Usuario usuarioLogado) {
+    public ModelAndView mostrarMapa(@AuthenticationPrincipal Usuario usuarioLogado, HttpServletRequest request) {
         ModelAndView model = new ModelAndView("map/mapa");
+
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            model.setViewName("redirect:/tarefa");
+            return model;
+        }
 
         model.addObject("usuario", usuarioLogado);
         return model;
