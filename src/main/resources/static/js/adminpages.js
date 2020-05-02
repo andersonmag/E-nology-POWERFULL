7a0d74d4163a6c791c871b9ee5908cd8a2afcbcf
Jackson Silva <jackson.silva.who@gmail.com>
2020-05-01 22:07:20 -0300

    Adição do método alterarSidebarr no adminpages.js

--- src/main/resources/static/js/adminpages.js
+++ src/main/resources/static/js/adminpages.js
@@ -6,3 +6,13 @@ for (var i = 0; i < links.length; i++) {
     if (links[i].href == location.href + "#")
         links[i].classList.add('ativo');
 }
+
+function alterarSidebarr(){
+    if (document.getElementById("sidenav").style.width == "200px") {
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("sidebar").style.width = "0";
+    }
+    else {
+        document.getElementById("sidenav").style.width = "200px";
+    }
+}
