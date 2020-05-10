37ed98b51c74e4af1ef79d347118134367acda74
Anderson <adnrepper@gmail.com>
2020-05-10 17:03:15 -0300

    Entidade e pacote Turma closed #53

---
+++ src/main/java/br/edu/ifal/enology/model/Turma.java
@@ -0,0 +1,52 @@
+package br.edu.ifal.enology.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Turma {
+    
+    @Id @GeneratedValue
+    private Long id;
+    private String semestre;
+    private String ano;
+
+    @ManyToMany
+    private List<Usuario> usuarios;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+}
