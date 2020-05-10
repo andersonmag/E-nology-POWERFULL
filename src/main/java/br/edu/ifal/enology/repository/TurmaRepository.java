0b92ef72dfe5d59103910b9b9fa986bdc39093f0
Anderson <adnrepper@gmail.com>
2020-05-10 17:07:11 -0300

    Repositorio da entidade turma

---
+++ src/main/java/br/edu/ifal/enology/repository/TurmaRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifal.enology.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import br.edu.ifal.enology.model.Turma;
+
+public interface TurmaRepository extends CrudRepository<Turma , Long>{
+
+}
