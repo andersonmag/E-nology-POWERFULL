8dbed99d5dac49a96dfb0b2d76e5385e7571cd9d
Anderson <adnrepper@gmail.com>
2020-05-13 13:22:17 -0300

    Herança agora de JpaRepository

--- src/main/java/br/edu/ifal/enology/repository/UserRepository.java
+++ src/main/java/br/edu/ifal/enology/repository/UserRepository.java
@@ -1,13 +1,8 @@
 package br.edu.ifal.enology.repository;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifal.enology.model.Usuario;
 
-public interface UserRepository extends CrudRepository<Usuario, Long>{
-
+public interface UserRepository extends JpaRepository<Usuario, Long>{
     Usuario findByEmail(String email);
-    List<Usuario> findAll();
-
 }
