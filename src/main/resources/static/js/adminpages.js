899b8d0d0a5ce778dbed278153823a13a90b8447
Anderson <adnrepper@gmail.com>
2020-05-13 13:37:31 -0300

    Inicio de reformução de css direto no elemento para classes em arquivos .css

--- src/main/resources/static/js/adminpages.js
+++ src/main/resources/static/js/adminpages.js
@@ -1,12 +1,4 @@
 
-var links = document.getElementsByName("link");
-
-for (var i = 0; i < links.length; i++) {
-
-    if (links[i].href == location.href + "#")
-        links[i].classList.add('ativo');
-}
-
 function alterarSidebarr(){
     if (document.getElementById("sidenav").style.width == "200px") {
         document.getElementById("sidenav").style.width = "0";
