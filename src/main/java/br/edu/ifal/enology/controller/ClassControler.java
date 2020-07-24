39e5066aee6ebd146b4ce683a79eaa744682192b
Anderson <adnrepper@gmail.com>
2020-07-24 00:58:38 -0300

    Service de Turma e instância no Controlador

--- src/main/java/br/edu/ifal/enology/controller/ClassControler.java
+++ src/main/java/br/edu/ifal/enology/controller/ClassControler.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.edu.ifal.enology.model.Turma;
 import br.edu.ifal.enology.model.Usuario;
 import br.edu.ifal.enology.repository.TurmaRepository;
 import br.edu.ifal.enology.repository.UserRepository;
+import br.edu.ifal.enology.service.TurmaService;
 import br.edu.ifal.enology.service.UsuarioService;
 
 @RestController
@@ -23,10 +25,15 @@ public class ClassControler {
 
     @Autowired
     TurmaRepository turmaRepository;
+
     @Autowired
     UsuarioService usuarioService;
+    
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    TurmaService turmaService;
 
     @RequestMapping
     public ModelAndView listarTurmas(@AuthenticationPrincipal Usuario usuario, ModelAndView model) {
@@ -51,24 +58,26 @@ public class ClassControler {
 
         model.setViewName("turma/turma-add");
         model.addObject("usuario", usuario).addObject("turma", new Turma());
-        return model;
+        return model; 
     }
 
     @RequestMapping("/{id}/add-alunos")
     public ModelAndView adicionarAlunosTurma(@PageableDefault(size = 5, page = 0) Pageable pageable, ModelAndView model,
             @AuthenticationPrincipal Usuario usuario, @PathVariable("id") Long id) {
+              Turma turmaOptional = turmaService.findById(id);
 
-        model.addObject("alunos", userRepository.findAll(pageable));
-        model.addObject("usuario", usuario);
-        model.addObject("turma", turmaRepository.findById(id).get());
-        model.setViewName("add-alunos");
+            model.addObject("alunos", userRepository.findAll(pageable));
+            model.addObject("usuario", usuario);
+            model.addObject("turma", turmaOptional);
+            model.setViewName("add-alunos");
 
-        return model;
+            return model;
+        
     }
 
     @RequestMapping("/{id}/add-alunos/{idAluno}")
     public ModelAndView salvarAlunoTurma(@PathVariable("idAluno") Long idAluno, @PathVariable("id") Long id,
-            @PageableDefault(size = 5, page = 0) Pageable pageable, ModelAndView model) {
+            @PageableDefault(size = 5, page = 0) Pageable pageable, ModelAndView model, RedirectAttributes redirect) {
 
         Turma turma = turmaRepository.findById(id).get();
         List<Usuario> usuarios = new ArrayList<>();
