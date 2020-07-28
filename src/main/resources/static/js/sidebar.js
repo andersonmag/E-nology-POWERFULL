9ebc2df26b1b2ed92d78f62b11d9691ded4b58e8
Anderson <adnrepper@gmail.com>
2020-07-28 19:55:04 -0300

    Atualizando página de erro 404

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -3,8 +3,11 @@ var links = document.getElementsByName("link");
 
 for (var i = 0; i < links.length; i++) {
 
-    if (links[i].href == location.href + "#")
+    if (links[i].href == location.href + "#"){
         links[i].classList.add('ativo');
+        break;
+    }
+        
 }
 
 function esconderSidebar() {
