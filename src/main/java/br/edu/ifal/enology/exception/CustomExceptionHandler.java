1b82f552a87c11a3669e17415c6042bb4f5b406f
Anderson <adnrepper@gmail.com>
2020-07-31 20:34:48 -0300

    Mensagem de erro e buttons alterados na página de erro

--- src/main/java/br/edu/ifal/enology/exception/CustomExceptionHandler.java
+++ src/main/java/br/edu/ifal/enology/exception/CustomExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,16 +16,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {    
 
-    @ExceptionHandler({ ClassNotFoundException.class, NoSuchElementException.class, NotFoundException.class })
+    @ExceptionHandler({ ClassNotFoundException.class, NoSuchElementException.class, NotFoundException.class, UsernameNotFoundException.class, BadCredentialsException.class })
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView handleNoValuePresent(Exception exception) {
         ModelAndView model = new ModelAndView("error");
-        HttpStatus status = HttpStatus.NOT_FOUND;
 
-        model.addObject("message", exception.getMessage()).addObject("time", LocalDateTime.now())
-                .addObject("status", status.value()).addObject("error", status.getReasonPhrase())
-                .addObject("details", exception);
-
+        model.setViewName("error");
+        model.addObject("msgErro", "O Link ou Recurso que você está buscando não existe, " +
+                        "ou pode estar expirado!");
         return model;
     }
 
