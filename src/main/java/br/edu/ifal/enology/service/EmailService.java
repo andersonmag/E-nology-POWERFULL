5cdee2545a39f8db5ab26d1695da122ff5970665
Anderson <adnrepper@gmail.com>
2020-07-31 20:41:04 -0300

    Metodo para enviar e-mail para redefinir senha

--- src/main/java/br/edu/ifal/enology/service/EmailService.java
+++ src/main/java/br/edu/ifal/enology/service/EmailService.java
@@ -11,7 +11,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import br.edu.ifal.enology.model.Usuario;
 
 @Service
@@ -47,15 +46,38 @@ public class EmailService{
     }
 
     public void enviarEmailConfirmacao(Usuario usuario){
+
+        String mensagem = "Bem vindo ao E-nology, " + usuario.getNome() + "! " +
+                          "\nPara ativar sua conta, use o código: \n" + usuario.getCodigoVerificacao() + 
+                          ".\nOu, clique no link: https://e-nology.herokuapp.com/ativar-conta/?codigoVerificacao=" + 
+                           usuario.getCodigoVerificacao() + "\n\nSe você não se cadastrou em nosso sistema, pedimos que desconsidere este email.";
+
         try {
-        enviarEmail("E-nology - Verificação de Email", usuario.getEmail(), "Bem vindo ao E-nology, " + usuario.getNome() + "! " +
-                    "\nPara ativar sua conta, use o código: \n" + usuario.getCodigoVerificacao() + 
-                    ".\nOu, clique no link: https://e-nology.herokuapp.com/ativar-conta/?codigoVerificacao=" + 
-                    usuario.getCodigoVerificacao() + "\n\nSe você não se cadastrou em nosso sistema, pedimos que desconsidere este email.");
+            enviarEmail("E-nology - Verificação de Email", usuario.getEmail(), mensagem);
+
         }catch(Exception e){
             System.err.println(e);
         }
     }
+    
+	public String enviarEmailRedefinirSenha(String token, Usuario usuario) {
+
+        String mensagem = "Olá, " + usuario.getNome() + "! Te ajudaremos a alterar sua senha.\n\n" + 
+                          "Para realizar a alteração de senha, basta clicar no link abaixo e redefini-la logo a seguir.\n" +
+                          "Segue o link para você alterar sua senha: " + "https://https://e-nology.herokuapp.com/redefinir-senha?tk=" + token +
+                          "\nNota: Este link espirará em 12 horas, ou caso efetue a mudança de senha." +
+                          "\n\nSe você não solicitou alteração de senha, pedimos que desconsidere este email.";
+
+        try {
+            enviarEmail("E-nology - Tudo Pronto para Você Redefinir Sua Senha!",
+                         usuario.getEmail(),mensagem);
+
+                return "E-mail Enviado! Verifique seu e-mail, por favor.";
+            }catch(Exception e){
+                System.err.println(e);
+                return "Não foi possivel concluir o envio de e-mail.";
+            }
+	}
 
     private Properties getProperties() {
 
