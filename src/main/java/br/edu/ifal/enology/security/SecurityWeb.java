b59371c3983299dacdc81e49c243ad39a53a5137
Jackson Silva <jackson.silva.who@gmail.com>
2020-09-29 22:57:04 -0300

    Acesso ao jogo apenas após o login

--- src/main/java/br/edu/ifal/enology/security/SecurityWeb.java
+++ src/main/java/br/edu/ifal/enology/security/SecurityWeb.java
@@ -23,7 +23,7 @@ public class SecurityWeb extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-                .antMatchers("/mapa", "/perfil", "/exercicio/**", "/atualizar", "/upload", "/cadastrar", "/sobre", "/siga-nos").authenticated()
+                .antMatchers("/mapa", "/perfil", "/exercicio/**", "/atualizar", "/upload", "/cadastrar", "/sobre", "/siga-nos", "/games/**").authenticated()
                 .antMatchers("/", "/cadastro", "/login").anonymous()
                 .antMatchers("/admin/**") .hasRole("ADMIN")
                 .and()
