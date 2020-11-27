3414fe66fa93aa4c4cb986cf5d1af1728038d24f
Jackson Silva <jackson.silva.who@gmail.com>
2020-11-27 17:04:09 -0300

    Enviando informações sobre o usuário logado para o minigame Yamato's Future

--- src/main/java/br/edu/ifal/enology/controller/GameController.java
+++ src/main/java/br/edu/ifal/enology/controller/GameController.java
@@ -2,15 +2,21 @@ package br.edu.ifal.enology.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifal.enology.model.Usuario;
 
 @RestController
 @RequestMapping("/games")
 public class GameController {
 
     @RequestMapping("/yamato-s-future")
-    public ModelAndView jogarYamatosFuture() {
-        return new ModelAndView("games/yamato");
+    public ModelAndView jogarYamatosFuture(@AuthenticationPrincipal Usuario usuarioLogado, ModelAndView model) {
+        model.setViewName("games/yamato");
+        model.addObject("usuario", usuarioLogado);
+
+        return model;
     }
 
     @RequestMapping("/mingle-ng")
