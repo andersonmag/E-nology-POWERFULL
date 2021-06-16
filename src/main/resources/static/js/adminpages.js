ba81a179cc3744cfe8f3bc120c7753177375904e
Jackson Silva <jackson.silva.who@gmail.com>
2021-06-15 23:03:57 -0300

    Edição na página lista-alunos, adminpages .css e .js

--- src/main/resources/static/js/adminpages.js
+++ src/main/resources/static/js/adminpages.js
@@ -12,3 +12,22 @@ function alterarSidebarr(){
 var ano = document.getElementById("ano");
 
 ano.value = '' + new Date().getFullYear();
+
+function changeSelection() {
+    let opt = document.getElementById("selection").value;
+    let fases = document.getElementById("fases");
+    let ranking = document.getElementById("ranking");
+
+    if (opt == "fases") {
+        changeClassList(ranking, "show", "hide");
+        changeClassList(fases, "hide", "show");
+    } else if (opt == "ranking") {
+        changeClassList(fases, "show", "hide");
+        changeClassList(ranking, "hide", "show");
+    }
+}
+
+function changeClassList(item, prev, cur) {
+    item.classList.remove(prev);
+    item.classList.add(cur);
+}
